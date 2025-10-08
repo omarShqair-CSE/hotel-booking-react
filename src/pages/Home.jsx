641aec490916aba3e6bd0d550ce3fae99ee62cbf
Omar AbuShqair <omarshqair854@gmail.com>
2025-10-08 11:32:43 +0300

    Add Testimonials component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import HotelCard from '../components/HotelCard'
 import Offers from '../components/Offers'
+import Testimonials from '../components/Testimonials'
 
 function Home() {
     return (
@@ -9,6 +10,7 @@ function Home() {
             <Hero />
             <HotelCard />
             <Offers />
+            <Testimonials />
         </>
     )
 }
